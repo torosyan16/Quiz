04a2718e6df5d5f7bfff7a74be4526cd14aebeac
SC-11 <torosyan4500@gmail.com>
2023-11-11 18:02:21 +0400

    new version. see in home

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,74 +2,99 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView totalQuestionsTextView;
-    TextView questionTextView;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView totalQuestionsTextView, questionTextView;
     Button ansAButton;
     Button ansBButton;
     Button ansCButton;
     Button ansDButton;
-    Button enterButton;
-
+    Button oKButton;
+    int totalQuestions = QuestionAnswer.question.length;
     String selectedAnswer = "";
-    int score = 0;
     int currentQuestionIndex = 0;
-
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ansAButton = findViewById(R.id.ans1);
-        ansBButton = findViewById(R.id.ans2);
-        ansCButton = findViewById(R.id.ans3);
-        ansDButton = findViewById(R.id.ans4);
+        totalQuestionsTextView = findViewById(R.id.total_question);
         questionTextView = findViewById(R.id.question);
-        enterButton = findViewById(R.id.enter);
+        ansAButton = findViewById(R.id.ans_a);
+        ansBButton = findViewById(R.id.ans_b);
+        ansCButton = findViewById(R.id.ans_c);
+        ansDButton = findViewById(R.id.ans_d);
+        oKButton = findViewById(R.id.ok);
+
+        // Set up click listeners
+        ansAButton.setOnClickListener(this);
+        ansBButton.setOnClickListener(this);
+        ansCButton.setOnClickListener(this);
+        ansDButton.setOnClickListener(this);
+        oKButton.setOnClickListener(this);
+        loadNewQuestion();
     }
 
     @Override
     public void onClick(View view) {
+        ansAButton.setBackgroundColor(Color.WHITE);
+        ansBButton.setBackgroundColor(Color.WHITE);
+        ansCButton.setBackgroundColor(Color.WHITE);
+        ansDButton.setBackgroundColor(Color.WHITE);
+
         Button button = (Button) view;
-        if (button.getId() == enterButton.getId()) {
-            if (selectedAnswer.equals(QuestionAnswers.answers[currentQuestionIndex])) {
-                ++score;
+        if (button.getId() == R.id.ok) {
+            if (selectedAnswer.equals(QuestionAnswer.answers[currentQuestionIndex])) {
+                score++;
             }
-            if (currentQuestionIndex == QuestionAnswers.questions.length - 1) {
-                resetQuestions();
-                return;
-            }
-            ++currentQuestionIndex;
-            resetQuestions();
+
+            currentQuestionIndex++;
             loadNewQuestion();
         } else {
-
             selectedAnswer = button.getText().toString();
             button.setBackgroundColor(Color.GREEN);
         }
     }
 
     private void loadNewQuestion() {
-        questionTextView.setText(QuestionAnswers.questions[currentQuestionIndex]);
-        ansAButton.setText(QuestionAnswers.choices[currentQuestionIndex][0]);
-        ansBButton.setText(QuestionAnswers.choices[currentQuestionIndex][1]);
-        ansCButton.setText(QuestionAnswers.choices[currentQuestionIndex][2]);
-        ansDButton.setText(QuestionAnswers.choices[currentQuestionIndex][3]);
+        if (currentQuestionIndex == totalQuestions){
+            finishQuiz();
+            return;
+        }
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansAButton.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansBButton.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansCButton.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansDButton.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
     }
 
-    private void resetQuestions() {
-        ansAButton.setBackgroundColor(Color.WHITE);
-        ansBButton.setBackgroundColor(Color.WHITE);
-        ansCButton.setBackgroundColor(Color.WHITE);
-        ansDButton.setBackgroundColor(Color.WHITE);
+    private void finishQuiz() {
+        String passStatus = "";
+        if (score > totalQuestions * 0.6){
+            passStatus = "Passed";
+        }else{
+            passStatus = "failed";
+        }
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is " + score + " out of " + totalQuestions)
+                .setPositiveButton("restart", ((dialogInterface, i) -> restartQuiz()))
+                .setCancelable(false)
+                .show();
     }
+
+    private void restartQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        loadNewQuestion();
+    }
+}
 }
--- app/src/main/java/com/example/quiz/QuestionAnswers.java
+++ app/src/main/java/com/example/quiz/QuestionAnswers.java
@@ -4,17 +4,19 @@ public class QuestionAnswers {
     public static String[] questions = {
             "How much is 1 + 1",
             "How much is 2 + 2",
-            "How much is 3 + 3"
+            "How much is 3 + 3",
+            "How much is 11 8 11"
     };
 
     public static String[][] choices = {
             {"1", "2", "3", "4"},
             {"2", "4", "6", "8"},
-            {"3", "6", "9", "12"}
+            {"3", "6", "9", "12"},
+            {"1/11", "11", "62", "121"}
     };
 
 
     public static String[] answers = {
-            "2", "4", "6"
+            "2", "4", "6", "121"
     };
 }
